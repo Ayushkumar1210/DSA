e1a1a3f7dbbe694d07cd3e2ec462ce6ea742c048
Ayush kumar <152275021+Ayushkumar1210@users.noreply.github.com>
2026-07-30 07:58:35 +0530

    Time: 0 ms (100%), Space: 27.3 MB (16.13%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -4,7 +4,9 @@ public:
         int start =0;
         int end=s.size()-1;
         while(start<=end){
-            swap(s[start++],s[end--]);
+            swap(s[start],s[end]);
+            start++;
+            end--;
         }
     }
   
